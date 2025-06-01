fc9b5ff7bce3a70ea9a234e5b83f1f55bae7eb6b
sugam ghising <ghisingsugam04@gmail.com>
2025-06-01 16:39:30 +0545

    fixed error issue in callList

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -55,7 +55,7 @@ const CallList = ({type}:{type: 'ended' |'upcoming'|'recordings'}) => {
         .flatMap((call) => call.recordings);
 
       setRecordings(recordings);
-    }catch(error){
+    }catch{
         toast('Try again later');
     }
     };
